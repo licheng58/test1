484e804b84ae816c9053da7cfc445df8d329f325
李成 <5627246+li_cheng58@user.noreply.gitee.com>
2021-04-12 22:29:23 +0800

    '88'

---
+++ src/components/text.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text">
+    <!-- <el-select :clearable="clearable" placeholder v-model="newValue">
+      <el-option label="草案" value="draft" />
+      <el-option label="发布" value="published" />
+      <el-option label="删除" value="deleted" />
+    </el-select>-->
+    <el-input v-model="newValue"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  value: {
+    type: String,
+    default() {
+      return ''
+    },
+  },
+  data() {
+    return {
+      // newValue: '',
+    }
+  },
+
+  computed: {
+    newValue: {
+      get: function() {
+        return this.value
+      },
+      set: function(value) {
+        this.$emit('changeval', value)
+      },
+    },
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,14 +3,31 @@
     home页面
     <div @click="toLogin">去login</div>
     <svg-icon class="icon" icon-class="home"></svg-icon>
+    <bbb @changeval="changeval" v-model="message"></bbb>
+    <div @click="abc" class="abc">{{message}}</div>
   </div>
 </template>
 
 <script>
+import bbb from '../../components/text.vue'
 export default {
+  components: {
+    bbb,
+  },
+  data() {
+    return {
+      message: 1,
+    }
+  },
   methods: {
     toLogin() {
       this.$router.push('/login')
+    },
+    changeval(v) {
+      this.message = v
+    },
+    abc() {
+      this.message++
     },
   },
 }
@@ -21,5 +38,10 @@ export default {
   width: 100px;
   height: 100px;
   color: red;
+}
+.abc {
+  width: 100px;
+  height: 100px;
+  border: 1px solid #000;
 }
 </style>
